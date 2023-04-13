f17898638621c46c8b201c329724d1bd99ca9b98
VanTrong4 <vantrong435@gmail.com>
2023-04-14 01:20:44 +0700

    first

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: ['min','max','opDefault'],
+    methods: {
+      getNumbers:function(){
+        let arr = [];
+        for (var i = this.min; i <= this.max; i++){
+            arr.push(i);
+        }
+        return arr
+      },
+      emits: ['update:modelValue'],
+}
+
+}
+</script>
+
+<template>
+    <select
+     class="focus:border-indigo-500 rounded-md shadow-sm mt-1 block h-[6.4rem] bg-gray-100 text-3xl border-none shrink grow"
+      @input="$emit('update:modelValue', $event.target.value)">
+        <option selected disabled>{{ opDefault }}</option>
+        <option v-for="n in getNumbers()" :value="n">{{ n }}</option>
+    </select>
+</template>
